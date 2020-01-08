8232931d9e1c557cf3abaef25152af00e76e3142
joeaniu <zengzhu.cn@gmail.com>
2020-01-08 17:00:55 +0800

    rename.

--- applications/yaml2zk/src/test/kotlin/net/thiki/eclab/yaml2zk/Yaml2ZkTest.kt
+++ applications/yaml2zk/src/test/kotlin/net/thiki/eclab/yaml2zk/Yaml2ZkTest.kt
@@ -23,4 +23,15 @@ internal class Yaml2ZkTest {
         CommandLine(Yaml2Zk()).execute("--help")
 
     }
+
+
+    @Test
+    @Disabled("depends on the zookeeper server.")
+    fun testMultipleDocument() {
+        val cut = Yaml2Zk()
+
+//        cut.run("path_to/test.yaml")
+        CommandLine(cut).execute("classpath:/test.yaml", "-r=/test")
+    }
+
 }
